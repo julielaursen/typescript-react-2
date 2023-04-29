3e9d188361f4a5f8a7368f4b0edaac06d02a2e39
julielaursen <julielaursen1@gmail.com>
2023-04-29 17:57:37 -0500

    video 300

--- rts/src/props/Child.tsx
+++ rts/src/props/Child.tsx
@@ -1,7 +1,24 @@
 interface ChildProps {
   color: string
+  onClick: () => void
+  children?: React.ReactNode
 }
 
-export const Child = ({ color }: ChildProps) => {
-  return <div>{color}</div>
+export const Child = ({ color, onClick }: ChildProps) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}>Click me</button>
+    </div>
+  )
+}
+
+// FC = Function Component
+export const ChildAsFC: React.FC<ChildProps> = ({ color, onClick }) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}>Click me</button>
+    </div>
+  )
 }
--- rts/src/props/Parent.tsx
+++ rts/src/props/Parent.tsx
@@ -1,7 +1,11 @@
 import { Child } from './Child'
 
 const Parent = () => {
-  return <Child color={'red'} />
+  return (
+    <Child color="red" onClick={() => console.log('clicked')}>
+      hello world
+    </Child>
+  )
 }
 
 export default Parent
