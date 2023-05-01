8d58d710133e611c3263b63a4ef5eee0eea94de0
julielaursen <julielaursen1@gmail.com>
2023-04-30 19:05:33 -0500

    finish project

--- rts/src/index.tsx
+++ rts/src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom'
-import UserSearch from '../src/state/UserSearch'
+import EventComponent from './events/EventComponent'
+import UserSearch from './refs/UserSearch'
 
 const App = () => {
   return (
